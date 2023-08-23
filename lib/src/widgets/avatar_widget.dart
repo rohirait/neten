9e897528afed7e7d8bae9ca4981832f874acb16c
Rait <rait@rendin.co>
2023-08-23 08:34:00 +0200

    Fix for avatars

--- lib/src/widgets/avatar_widget.dart
+++ lib/src/widgets/avatar_widget.dart
@@ -4,8 +4,19 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../providers/client_provider.dart';
 
 class AvatarWidget extends ConsumerWidget {
+  final List<String> acceptedAvatarUrls = [
+    'avatar1.jpg',
+    'avatar2.jpg',
+    'avatar3.jpg',
+    'avatar4.jpg',
+    'avatar5.jpg',
+    'avatar6.jpg',
+    'avatar7.jpg',
+    'avatar8.jpg',
+    'avatar9.jpg',
+    'avatar10.jpg'
+  ];
 
-  final List<String> acceptedAvatarUrls = ['avatar1.png', 'avatar2.png', 'avatar3.png', 'avatar4.png', 'avatar5.png', 'avatar6.png'];
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final client = ref.watch(clientProvider);
@@ -13,7 +24,7 @@ class AvatarWidget extends ConsumerWidget {
     return CircleAvatar(
       backgroundImage: client.value != null && client.value!.url != null && !acceptedAvatarUrls.contains(client.value!.url)
           ? NetworkImage(client.value!.url!)
-          : AssetImage('assets/img/'+(client.value?.url != null ? client.value!.url! : 'random.png')) as ImageProvider<Object>,
+          : AssetImage('assets/img/' + (client.value?.url != null ? client.value!.url! : 'random.jpg')) as ImageProvider<Object>,
       radius: 50,
     );
   }
